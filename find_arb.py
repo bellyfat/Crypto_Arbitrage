38375f446f775d0ae5c6235071c10df8732bc140
Anmol Agrawal <anmolagrawal@Anmols-MBP.attlocal.net>
2021-12-21 21:59:33 -0600

    added csv persistence

--- find_arb.py
+++ find_arb.py
@@ -1,11 +1,13 @@
 from shortest_path import get_arb_path
 from construct_matrix import get_price_matrix
 import time
+import numpy as np
 
 i = 0
 while True:
 	print('trial number {}'.format(i))
 	p_matrix = get_price_matrix()
+	numpy.savetxt('output.csv',np.asarray(p_matrix),delimiter=",")
 	found_arb = get_arb_path(p_matrix, 0)
 	if found_arb:
 		print('FOUND AN ARB!!!!!!!!')
